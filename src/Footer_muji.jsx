4fe80061db2481bddc97d5ddd3f9182593f6d8db
seomiging <annamingki@gmail.com>
2026-03-26 13:01:57 +0900

    0326_footer

--- src/Footer_muji.jsx
+++ src/Footer_muji.jsx
@@ -3,9 +3,59 @@ import './css/footer.css'
 
 const Footer_muji = () => {
   return (
+    <footer>
+    <ul>
+    <li>AboutMUJI</li>
+    <li>채용공고</li>
+    <li>매장안내</li>
+    <li>이용약관</li>
+    <li className='footer_li_private'>개인정보처리방침</li>
+    </ul>
     <div>
-      이건 푸터
+      <div className='footer_logobox'><img src='./footer/logo.svg'/></div>
+      <div className='footer_text_innerbox'>
+        <div className='footertext1'>
+          <ul>
+            <li>고객센터</li>
+            <li>공지사항</li>
+            <li>1:1문의</li>
+            <li>제휴문의</li>
+          </ul>
+        </div>
+        <div className='footertext2'>
+          <p>상호명 : 무인양품㈜</p>
+          <p>대표 : NINOMIYA KENICHIRO,김진엽</p>
+          <p>주소 : 서울 중랑구 서소문로 50 10층</p>
+          <p>(중림동,CENTRALPLACE)</p>
+          <p>사업자 번호 105-86-73836</p>
+          <p>통신판매업신고 : 2024-서울중구-1904</p>
+          <div>
+            <img src='./footer/play.webp'alt='play'/>
+            <img src='./footer/apple.svg' alt='apple'/>
+            <img src='./footer/insta.svg' alt='insta'/>
+            <img src='./footer/facebook.svg' alt='facebook'/>
+            <img src='./footer/youtube.svg' alt='youtube'/>
+          </div>
+        </div>
+        
+        <div className='footertext3'>
+          <p>고객만족센터 : 1551-3780</p>
+          <p>운영시간 : 평일 09:30 ~ 18:30(주말, 공휴일 휴무)</p>
+          <p>점심시간 : 12:30 ~ 13:30</p>
+          <br/>
+          <p>고객님의 안전거래를 위해 현금 등으로 결제 시 결제금액의 전액에<br/>
+          대해 저희 쇼핑몰에서 가입한 소비자 피해 보상보험 서비스를 <br/>
+          이용하실 수 있습니다.</p>
+          <br/>
+          <p>임직원 인증</p>
+
+          <div>
+            <p>GLOBAL SITE　＞</p>
+          </div>
+        </div>
+      </div>
     </div>
+    </footer>
   )
 }
 
