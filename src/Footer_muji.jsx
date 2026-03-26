d93887309b3f1ee78dd1fd71c5d6419efc8278a6
seomiging <annamingki@gmail.com>
2026-03-26 14:25:01 +0900

    footer_

--- src/Footer_muji.jsx
+++ src/Footer_muji.jsx
@@ -4,7 +4,8 @@ import './css/footer.css'
 const Footer_muji = () => {
   return (
     <footer>
-    <ul>
+      <div className='footerinner'>
+    <ul className='footerul'>
     <li>AboutMUJI</li>
     <li>채용공고</li>
     <li>매장안내</li>
@@ -29,12 +30,12 @@ const Footer_muji = () => {
           <p>(중림동,CENTRALPLACE)</p>
           <p>사업자 번호 105-86-73836</p>
           <p>통신판매업신고 : 2024-서울중구-1904</p>
-          <div>
-            <img src='./footer/play.webp'alt='play'/>
-            <img src='./footer/apple.svg' alt='apple'/>
-            <img src='./footer/insta.svg' alt='insta'/>
-            <img src='./footer/facebook.svg' alt='facebook'/>
-            <img src='./footer/youtube.svg' alt='youtube'/>
+          <div className='footertext2_imgbox'>
+           <div> <img src='./footer/play.webp'alt='play'/></div>
+            <div><img src='./footer/apple.svg' alt='apple'/></div>
+            <div><img src='./footer/insta.svg' alt='insta'/></div>
+            <div><img src='./footer/facebook.svg' alt='facebook'/></div>
+           <div><img src='./footer/youtube.svg' alt='youtube'/></div>
           </div>
         </div>
         
@@ -47,13 +48,19 @@ const Footer_muji = () => {
           대해 저희 쇼핑몰에서 가입한 소비자 피해 보상보험 서비스를 <br/>
           이용하실 수 있습니다.</p>
           <br/>
-          <p>임직원 인증</p>
+          <p className='footertext3_check'>임직원 인증</p>
 
-          <div>
-            <p>GLOBAL SITE　＞</p>
+          <div className='footerbtn'>
+            <button className='footer_btn'><p>GLOBAL SITE　＞</p></button>
           </div>
         </div>
       </div>
+    </div>
+    </div>
+
+    <div className='footertext_mob'>
+    <p>무인양품㈜ 사업자정보 ∨</p>
+    <p>COPYRIGHT ㈜무인양품 ALL RIGHTS RESERVED.</p>
     </div>
     </footer>
   )
